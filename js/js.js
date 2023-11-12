bd65ca7b37f5d58841e5d3ab43f1a884d283b25f
unknown <Baxtiyorddushaev@gmail.com>
2023-11-12 13:43:38 +0500

    first

---
+++ js/js.js
@@ -0,0 +1,83 @@
+
+
+// let monthEnterName = prompt('Enter the name of month', ''),
+// 	monthNum = +prompt('Enter the number of month', '');
+
+
+// monthName();
+
+// function monthName() {
+
+// 	if (monthNum == 30) {
+// 		for (let i = 0; i <= 30; i++) {
+// 			console.log(monthEnterName, i);
+// 		}
+// 	} else if (monthNum > 28 && monthNum == 31) {
+// 		for (let i = 0; i <= 31; i++) {
+// 			console.log(monthEnterName, i);
+// 		}
+// 	} else if (monthNum == 28) {
+// 		for (let i = 0; i <= 28; i++) {
+// 			console.log(monthEnterName, i);
+// 		}
+// 	} else if (monthNum == 29) {
+// 		for (let i = 0; i <= 29; i++) {
+// 			console.log(monthEnterName, i);
+// 		}
+// 	}
+// 	else {
+// 		console.log('Something is false');
+// 	}
+// }
+let monthEnterName = prompt('Enter the name of month', ''),
+	monthNum = +prompt('Enter the number of month', '');
+
+
+monthName();
+function monthName() {
+	console.log(monthEnterName);
+	for (let i = 0; i <= 28; i++) {
+		console.log(i);
+	}
+}
+// function monthName() {
+// 	console.log(monthEnterName);
+// 	for (let i = 0; i <= 28; i++) {
+// 		console.log(i);
+// 	}
+// 	if (monthNum == 29) {
+// 		console.log(29);
+// 	} else if (monthNum == 30) {
+// 		console.log(29);
+// 		console.log(30);
+// 	} else if (monthNum == 31) {
+// 		console.log(29);
+// 		console.log(30);
+// 		console.log(31);
+// 	} else {
+// 		console.log('Something is false');
+// 	}
+// }
+
+// if (monthNum == 30) {
+// 	for (let i = 0; i <= 30; i++) {
+// 		console.log(monthEnterName, i);
+// 	}
+// } else if (monthNum > 28 && monthNum == 31) {
+// 	for (let i = 0; i <= 31; i++) {
+// 		console.log(monthEnterName, i);
+// 	}
+// } else if (monthNum == 28) {
+// 	for (let i = 0; i <= 28; i++) {
+// 		console.log(monthEnterName, i);
+// 	}
+// } else if (monthNum == 29) {
+// 	for (let i = 0; i <= 29; i++) {
+// 		console.log(monthEnterName, i);
+// 	}
+// }
+// else {
+// 	console.log('Something is false');
+// } }
+
+
